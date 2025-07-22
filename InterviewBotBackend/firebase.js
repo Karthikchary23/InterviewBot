c785c2e058ba809d539cb673f23393e095965883
Karthikcahry23 <lingojikarthikchary@gmail.com>
2025-07-22 14:45:23 +0530

    final commit

--- InterviewBotBackend/firebase.js
+++ InterviewBotBackend/firebase.js
@@ -1,5 +1,7 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebaseServiceAccount.json" with { type: "json" };
+// import serviceAccount from "./firebaseServiceAccount.json" with { type: "json" };
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
